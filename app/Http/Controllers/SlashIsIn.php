d65588d06acf02358c74904a07af88b0260a51ba
jakebathman <jake.bathman@gmail.com>
2019-03-29 16:30:50 -0500

    Add Slack message class and some helper methods

--- app/Http/Controllers/SlashIsIn.php
+++ app/Http/Controllers/SlashIsIn.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\SlackClient;
+use App\SlackMessage;
 use App\SlackUser;
 use DateTime;
 use Illuminate\Http\Request;
@@ -13,6 +14,7 @@ use Wgmv\SlackApi\Facades\SlackUser as SlackUserClient;
 
 class SlashIsIn extends Controller
 {
+    protected $replyMessage;
 
     public function __construct($teamId = null)
     {
@@ -20,6 +22,7 @@ class SlashIsIn extends Controller
         $this->teamId = $teamId ?? config('services.slack.team_id');
         $this->client = new SlackClient($this->teamId);
         $this->users = $this->client->getUsers();
+        $this->replyMessage = new SlackMessage;
     }
 
     public function __invoke(Request $request)
@@ -31,11 +34,11 @@ class SlashIsIn extends Controller
         // Slack escapes @mentions to look like <@U012ABCDEF>
         $pattern = "/\<@([\A-Z0-9]+)(?:\|[\w]+)?\>/";
         if (! preg_match_all($pattern, $message, $mentions)) {
-            return "Make sure you're including a username, like */IsIn @someone*";
+            return $this->reply("Make sure you're including a username, like */IsIn @someone*");
         }
 
         if (count($mentions[1]) > 1) {
-            return "Only mention one person, so I know who you're looking for! E.g. */IsIn @someone*";
+            return $this->reply("Only mention one person, so I know who you're looking for! E.g. */IsIn @someone*");
         }
 
         $userMentionedId = $mentions[1][0];
@@ -52,17 +55,26 @@ class SlashIsIn extends Controller
         $statusData = (new GetStaffIn())->getStatuses();
 
         if (array_get($statusData, 'status') != 'success') {
-            return "Sorry, something went wrong trying to look that up. Here's the error message:\n> " . array_get($statusData, 'message', '(No error message)');
+            return $this->reply(
+                "Sorry, something went wrong trying to look that up. Here's the error message:\n> "
+                . array_get($statusData, 'message', '(No error message)')
+            );
         }
 
         $statuses = collect(array_get($statusData, 'data.statuses'));
 
         // Get the status of the mentioned person
         if ($info = $statuses->get($userMentionedId)) {
-            return "@{$info['display_name']} is *@{$info['status']}*. Their last message in #general was {$info['since']}:\n> {$info['last_message']}";
-            $statuses->get($userMentionedId);
+            return $this->reply("@{$info['display_name']} is *@{$info['status']}*. Their last message in #general was {$info['since']}:\n> {$info['last_message']}");
         }
 
-        return "I've not seen @{$info['display_name']} in #general yet today, so you can assume they're *@out* right now.";
+        return $this->reply("I've not seen @{$info['display_name']} in #general yet today, so you can assume they're *@out* right now.");
+    }
+
+    protected function reply($text)
+    {
+        return $this->replyMessage
+            ->text($text)
+            ->toString();
     }
 }
