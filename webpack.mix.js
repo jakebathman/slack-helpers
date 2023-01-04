d6d50455e911f52f2513884f8a323108c13d7872
Jake Bathman <jake.bathman@gmail.com>
2023-01-04 10:12:18 -0600

    Fix npm scripts and assets for node 18

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,6 @@ let mix = require('laravel-mix')
 
 mix.js('resources/js/app.js', 'public/js')
   .postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
     require('tailwindcss'),
   ]);
 
